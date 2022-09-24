1ae89539aa2650238c0a352f80ab57d40370d878
DanielCosta12 <danieldossantosdacosta@gmail.com>
2022-09-24 19:25:53 -0300

    add new api

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const submit = document.querySelector("#submit");
 async function getData() {
   try {
     let ceps = document.querySelector("#cep").valueAsNumber;
-    const res = await fetch(`https://cep.awesomeapi.com.br/${ceps}`);
+    const res = await fetch(`https://brasilapi.com.br/api/cep/v1/${ceps}`);
     if (res.status != 200) {
-      return alert("C");
+      return alert(
+        "Adicione o cep correto ou a API está com problemas se você digitou o cep correto!"
+      );
     } else {
       const data = await res.json();
       addHtml(data);
@@ -16,8 +18,8 @@ async function getData() {
 }
 function addHtml(data) {
   let result = document.querySelector("#result");
-  result.textContent = `Endereço: ${data.address}
-   - Seu Bairro: ${data.district}
+  result.textContent = `Endereço: ${data.street}
+   - Seu Bairro: ${data.neighborhood}
    - Sua Cidade/Estado: ${data.city}/${data.state}
    - e seu cep é: ${data.cep}`;
 }
